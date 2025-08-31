fef1066c656832ed57284ec95e2d0edb0f2c1ae3
Harpo MAxx <harpomaxx@gmail.com>
2025-08-31 11:30:24 -0300

    change export script to new game name

--- export_game.sh
+++ export_game.sh
@@ -8,15 +8,15 @@ echo "Starting Dollhouse Leak Fixer export process..."
 # Copy the game to PICO-8 carts directory
 echo "Copying game to PICO-8 carts directory..."
 mkdir -p /home/harpo/.lexaloffle/pico-8/carts/
-cp dollhouse_leak_fixer.p8 /home/harpo/.lexaloffle/pico-8/carts/
+cp iueve.p8 /home/harpo/.lexaloffle/pico-8/carts/
 
 # Copy existing label if it exists
 echo "Looking for existing label..."
 LABEL_FILE=$(find /home/harpo/.lexaloffle/pico-8/ -maxdepth 1 -name "*_label.png" | head -1)
-if [ -n "$LABEL_FILE" ] && [ "$LABEL_FILE" != "/home/harpo/.lexaloffle/pico-8/dollhouse_leak_fixer_label.png" ]; then
+if [ -n "$LABEL_FILE" ] && [ "$LABEL_FILE" != "/home/harpo/.lexaloffle/pico-8/iueve_label.png" ]; then
     echo "Copying existing label: $LABEL_FILE"
-    cp "$LABEL_FILE" /home/harpo/.lexaloffle/pico-8/carts/dollhouse_leak_fixer_label.png
-    cp "$LABEL_FILE" /home/harpo/.lexaloffle/pico-8/dollhouse_leak_fixer_label.png
+    cp "$LABEL_FILE" /home/harpo/.lexaloffle/pico-8/carts/iueve_label.png
+    cp "$LABEL_FILE" /home/harpo/.lexaloffle/pico-8/iueve_label.png
 else
     echo "Label already exists or no label file found"
 fi
@@ -28,11 +28,11 @@ mkdir -p js
 echo "Running PICO-8 export..."
 cd ~/Downloads/pico-8
 # First create a .p8.png with the label, then export to HTML
-./pico8 /home/harpo/.lexaloffle/pico-8/carts/dollhouse_leak_fixer.p8 -export dollhouse_temp.p8.png
-if [ -f "dollhouse_temp.p8.png" ]; then
+./pico8 /home/harpo/.lexaloffle/pico-8/carts/iueve.p8 -export iueve_temp.p8.png
+if [ -f "iueve_temp.p8.png" ]; then
     echo "Created .p8.png, now exporting to HTML..."
-    ./pico8 dollhouse_temp.p8.png -export iueve.html
-    rm dollhouse_temp.p8.png
+    ./pico8 iueve_temp.p8.png -export iueve.html
+    rm iueve_temp.p8.png
 else
     echo "Failed to create .p8.png file"
 fi
